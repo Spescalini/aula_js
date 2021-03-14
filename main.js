132d79ae5811dca74244e2bf153329ca7a367253
unknown <sergiopescalini@gmail.com>
2021-03-14 19:34:42 -0300

    Inicial

---
+++ main.js
@@ -0,0 +1,23 @@
+var frutas = [{nome: "Maçã", cor: "Vermelho", price: "R$ 6,99", unidade: "KG"}, {nome: "Uva", cor: "Roxa", price: "R$ 12,99", unidade: "KG"}]
+console.log(frutas);
+alert(frutas[0].price);
+
+/*var fruta = {nome: "Maçã", cor: "Vermelho", price: "R$ 6,99", unidade: "KG"};
+console.log(fruta);
+alert(fruta.price);*/
+
+/*var nome ="Sérgio Pescalini";
+var n1 = 28;
+var n2 = 10;
+var frase = "Japão é o melhor time deo mundo"
+//alert("Bem Vindo" + nome);
+//alert(idade+idade2);
+console.log(nome);
+console.log(n1*n2);
+console.log(frase.replace("Japão", "Brasil"));
+console.log(frase.toLowerCase());*/
+
+/*var lista = ["Maça", "Pêra", "Laranja"];
+lista.push("Uva");
+console.log(lista.toString());
+console.log(lista.join(" || "));*/
